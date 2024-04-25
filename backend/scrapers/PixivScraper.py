bfb7204f1c090a386b5694e104791275d44a8bf8
Jessica Ho <jho@Jessicas-MBP.attlocal.net>
2024-04-24 22:48:37 -0700

    smol cleanup

--- backend/scrapers/PixivScraper.py
+++ backend/scrapers/PixivScraper.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 import contextlib
-from typing import List, Any, Union
 # import requests
 from urllib.parse import urlparse
 # from PIL import Image
@@ -31,13 +30,10 @@ class PixivScraper(Scraper):
     self.episode = urlparse(url).path.split('/')[-1]
 
   async def __get_blob_urls(self, page: Page):
-    try:
-      return await page.locator('div[id^="page-"]').evaluate_all(
-        '(divs) => divs.map((div) => div.style.backgroundImage.match(/url\("(.*?)"/)?.[1])'
-      )
-    except: 
-      print('SOMETHING WENT WRONG WITH GETTING BLOB URLS')
-      return []
+    return await page.locator('div[id^="page-"]').evaluate_all(
+      '(divs) => divs.map((div) => div.style.backgroundImage.match(/url\("(.*?)"/)?.[1])'
+    )
+
   async def __wait_for_blobs(self, evt, timeout):
     # suppress TimeoutError because we'll return False in case of timeout
     with contextlib.suppress(asyncio.TimeoutError):
@@ -54,22 +50,18 @@ class PixivScraper(Scraper):
     def handle_response(response: Response):
       url = response.url
       if url.startswith('blob'):
-        print('GOT BLOB')
         blob_responses[url] = response
 
     page.on("response", handle_response)
-    print('before goto')
     await page.goto(self.url, wait_until='networkidle')
-    print('after goto')
 
-    print('before get_blob_urls')
     blob_urls = await self.__get_blob_urls(page)
-    print('after get_blob_urls')
 
     all_blobs_evt = asyncio.Event()
-    print('before wait_for_blobs')
     awaited_blobs = set(blob_urls)
-    while not await self.__wait_for_blobs(all_blobs_evt, 5):
+    # honestly this bit about waiting for blobs might redundant
+    # when networkidle, there should be no more blobs coming in
+    while not await self.__wait_for_blobs(all_blobs_evt, 3):
         to_delete = []
         for blob_url in awaited_blobs:
           if blob_url in blob_responses:
@@ -77,28 +69,19 @@ class PixivScraper(Scraper):
         
         for blob_url in to_delete:
           awaited_blobs.remove(blob_url)
-        print(len(blob_urls), len(awaited_blobs))
-        # if len(blob_urls) == len(blob_responses):
-        if len(blob_urls) > len(awaited_blobs): 
+        if len(awaited_blobs) == 0: 
           all_blobs_evt.set()
     
-    print('GOT ALL BLOBS!')
-    print('DESIRED: ', blob_urls)
-    print('GOT: ', blob_responses.keys())
     buffers: list[bytes] = []
     for page_num, blob_url in enumerate(blob_urls):
       if not page_num or blob_url not in blob_responses: # skip first page since it's empty
         continue
       
       response = blob_responses[blob_url]
-      print('ADDED BUFFER', blob_url)
       buffers.append(await response.body())
 
-    print('ALL BUFFERS ADDED')
     await context.close()
-    print('CLOSED CONTEXT')
     await browser.close()
-    print('CLOSED BROWSER')
     return buffers
 
   async def first_scrape(self):
