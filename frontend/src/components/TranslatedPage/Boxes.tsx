fdfc715edecf803c5b2561e7053f0e708d93e3cf
Jessica Ho <jho@Jessicas-MBP.attlocal.net>
2024-05-01 02:49:37 -0700

    tentatively fixed drag resize

--- frontend/src/components/TranslatedPage/Boxes.tsx
+++ frontend/src/components/TranslatedPage/Boxes.tsx
@@ -3,6 +3,11 @@ import Moveable from "react-moveable";
 import { useViewer } from "../../contexts/Viewer";
 import { Block } from "../../lib/definitions";
 
+const parseTransform = (transform: string | undefined) => {
+  return (
+    transform?.match(/-?\d+(\.\d+)?/g)?.map((x) => parseFloat(x)) ?? [0, 0]
+  );
+};
 export const TranslatedBox = ({
   block,
   pageNum,
@@ -19,7 +24,7 @@ export const TranslatedBox = ({
   const [hovering, setHovering] = useState(false);
 
   /* Refs */
-  const targetRef = useRef<HTMLTextAreaElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
   const moveableRef = useRef<Moveable>(null);
 
   /* Computed */
@@ -28,44 +33,89 @@ export const TranslatedBox = ({
   const PADDING = 10;
 
   return (
-    <div
-      onMouseEnter={() => setHovering(true)}
-      onMouseLeave={() => setHovering(false)}
-      className="absolute pointer-events-auto"
-      style={{
-        width: `${width + PADDING * 2}px`,
-        height: `${height + PADDING * 2}px`,
-        left: `${block.box[0] - PADDING}px`,
-        top: `${block.box[1] - PADDING}px`,
-        border: `${PADDING}px solid transparent`,
-        zIndex: blockNum,
-      }}
-    >
-      {/* TODO: replace with contenteditable div so can vertically center text */}
-      <textarea
-        ref={targetRef}
-        onChange={(e) => {
-          handleTextEdit({
-            pageNum,
-            blockNum,
-            val: e.currentTarget.value,
-          });
+    <div className="pointer-events-auto">
+      <div
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={(e) => {
+          if (
+            (e.relatedTarget as HTMLElement)?.classList?.contains(
+              "moveable-direction"
+            )
+          )
+            return;
+          setHovering(false);
         }}
-        onClick={(e) => {
-          e.currentTarget.focus();
+        className="absolute"
+        style={{
+          width: `${width + PADDING * 2}px`,
+          height: `${height + PADDING * 2}px`,
+          left: `${block.box[0] - PADDING}px`,
+          top: `${block.box[1] - PADDING}px`,
+          border: `${PADDING}px solid red`,
+          // width: `${width}px`,
+          // height: `${height}px`,
+          // left: `${block.box[0]}px`,
+          // top: `${block.box[1]}px`,
+          // border: `${PADDING}px solid red`,
+          // boxSizing: "border-box",
+
+          zIndex: blockNum,
+          transform: block.transform?.transform,
+          background: "pink",
         }}
-        className="bg-white text-black overflow-hidden resize-none text-center"
-        defaultValue={block.lines[0]}
+      >
+        <div
+          ref={targetRef}
+          style={{
+            width: `${width}px`,
+            height: `${height}px`,
+            background: "blue",
+          }}
+          className="flex justify-center items-center pointer-events-auto"
+        >
+          <div className="max-w-full max-h-full ">Target</div>
+        </div>
+        {/* <div
+          style={{
+            width: `${width}px`,
+            height: `${height}px`,
+          }}
+          className="overflow-hidden bg-white flex justify-center items-center"
+        >
+          <div
+            onBlur={(e) => {
+              handleTextEdit({
+                pageNum,
+                blockNum,
+                val: e.currentTarget.innerText,
+              });
+            }}
+            onClick={(e) => {
+              e.currentTarget.focus();
+            }}
+            className="text-black text-center max-w-full max-h-full outline-0"
+            defaultValue={block.lines[0]}
+            contentEditable={true}
+            suppressContentEditableWarning={true}
+        style={{
         style={{
           width: `${width}px`,
           height: `${height}px`,
           transform: block.transform?.transform,
-          fontSize: `${block.font_size}px`,
-          writingMode: block.vertical
-            ? ("vertical-rl" as const)
-            : ("unset" as const),
-        }}
-      />
+            style={{
+          width: `${width}px`,
+          height: `${height}px`,
+          transform: block.transform?.transform,
+              fontSize: `${block.font_size}px`,
+              writingMode: block.vertical
+                ? ("vertical-rl" as const)
+                : ("unset" as const),
+            }}
+          >
+            {block.lines[0]}
+          </div>
+        </div> */}
+      </div>
       <Moveable
         hideDefaultLines={!hovering}
         origin={false}
@@ -80,27 +130,73 @@ export const TranslatedBox = ({
         renderDirections={
           hovering ? ["nw", "n", "ne", "w", "e", "sw", "s", "se"] : []
         }
+        onDragEnd={(e) => {
+          if (e.target.parentElement) {
+            const [pDx, pDy] = parseTransform(
+              e.target.parentElement.style.transform
+            );
+            const [cDx, cDy] = parseTransform(e.target.style.transform);
+
+            const transform = `translate(${pDx + cDx}px, ${pDy + cDy}px)`;
+            e.target.parentElement.style.transform = transform;
+            e.target.style.transform = "";
+
+            handleBoxDragResize({
+              pageNum,
+              blockNum,
+              transform,
+              width,
+              height,
+            });
+          }
+        }}
         onDrag={(e) => {
+          // const [dx, dy] = parseTransform(e.transform);
+          // const [prevDx, prevDy] = parseTransform(
+          //   e.target.parentElement?.style.transform
+          // );
+          // if (e.target.parentElement) {
+          //   e.target.parentElement.style.transform = `translate(${
+          //     prevDx + dx
+          //   }px, ${prevDy + dy}px)`;
+          // }
+
+          // if (e.target.parentElement)
+          //   e.target.parentElement.style.transform = e.transform;
           e.target.style.transform = e.transform;
-          handleBoxDragResize({
-            pageNum,
-            blockNum,
-            transform: e.transform,
-            width: e.width,
-            height: e.height,
-          });
+        }}
+        onResizeEnd={(e) => {
+          const { width: newWidthPx, height: newHeightPx } = e.target.style;
+          const newWidth = parseFloat(newWidthPx);
+          const newHeight = parseFloat(newHeightPx);
+          if (e.target.parentElement) {
+            e.target.parentElement.style.width = `${newWidth + PADDING * 2}px`;
+            e.target.parentElement.style.height = `${
+              newHeight + PADDING * 2
+            }px`;
+
+            const [pDx, pDy] = parseTransform(
+              e.target.parentElement.style.transform
+            );
+            const [cDx, cDy] = parseTransform(e.target.style.transform);
+
+            const transform = `translate(${pDx + cDx}px, ${pDy + cDy}px)`;
+            e.target.parentElement.style.transform = transform;
+            e.target.style.transform = "";
+
+            handleBoxDragResize({
+              pageNum,
+              blockNum,
+              transform,
+              width: newWidth,
+              height: newHeight,
+            });
+          }
         }}
         onResize={(e) => {
           e.target.style.width = `${e.width}px`;
           e.target.style.height = `${e.height}px`;
           e.target.style.transform = e.drag.transform;
-          handleBoxDragResize({
-            pageNum,
-            blockNum,
-            transform: e.transform,
-            width: e.width,
-            height: e.height,
-          });
         }}
       />
     </div>
